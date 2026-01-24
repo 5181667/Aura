423301d463f56c26281a5f3b0535b164201d0eec
5181667 <2424742061@qq.com>
2026-01-24 21:23:39 +0800

    seo

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,9 @@
 import { MetadataRoute } from 'next'
 import { prisma } from '@/lib/prisma'
 
+export const runtime = 'nodejs'
+export const dynamic = 'force-dynamic'
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     const siteUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://hhcc.online'
 
